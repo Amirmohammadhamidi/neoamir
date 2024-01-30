ecd89354cd6ae481ec8ef43e1068395e34fd25e6
amirmohammadhamidi <amirmohammadhmd.20@gmail.com>
2024-01-30 16:51:09 +0330

    initialization

---
+++ neogit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<time.h>
+#include<dirent.h>
+int main(int argc , char * argv[]){
+    printf("%s",argv[1]);
+
+
+
+
+    return 0;
+}
